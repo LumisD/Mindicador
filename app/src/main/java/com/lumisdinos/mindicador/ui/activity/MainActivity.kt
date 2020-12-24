24bd1698a63d47935d0ab9f9e6f8a1114baa17d2
LumisD <lumisdinos@gmail.com>
2020-12-24 16:48:53 -0300

    Extend activity from DaggerAppCompatActivity

--- app/src/main/java/com/lumisdinos/mindicador/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lumisdinos/mindicador/ui/activity/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.lumisdinos.mindicador.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.lumisdinos.mindicador.R
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.navListHome)
+                .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
